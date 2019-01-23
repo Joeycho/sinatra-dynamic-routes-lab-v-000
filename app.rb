3089d4b48ded068c9c730453286a7adce9738915
Joey Cho <cjy12182000@naver.com>
2019-01-23 06:30:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,6 +38,14 @@ class App < Sinatra::Base
     @word4 = params[:word4]
     @word5 = params[:word5]
     
+    @str =""
+    
+    "#{5.times do |i|
+      @str+=@word#{i}+"."
+    }"
+    
+    @str
+    
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   
   end
